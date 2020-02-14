2bb2be54032b399d4d0a501f2ace7bb1257016b1
Kishan Teli <48146922+KishanTeli@users.noreply.github.com>
2020-02-13 21:55:35 -0500

    Add files via upload

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
-
+"""
+Kishan Teli
+"""
 """
 Python Core object Types
 """
 
 import math
+
 
 def numbers_and_strings():
     """
@@ -14,14 +17,21 @@ def numbers_and_strings():
     x = "EE551"
 
     # Assign a string "Stevens" to a variable y
+    y = "Stevens"
 
     # Repeat variable y 5 times
+    print(y*5)
 
     # What is the length of z?
+    z = ""
+    length = len(z)
 
     # Concatenate variable y with string " is good"
+    y = y + ' is good'
 
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
+    m = "good"
+    n = m.replace('good','awesome')
 
     return x, y, z, length, m, n
 
@@ -33,21 +43,30 @@ def lists():
     n = "Stevens is awesome"
 
     # Split variable n on a delimiter space into a list of substrings
+    p = n.split()
 
     # Get all the items past the first of the third substring
-
+    r = p[:-1]
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
+    A= [[1,4,5], [6,10,11],[12,17,38]]
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            print(A[i][j], end=' ')
+        print()
 
     # Collect the items in the last column of matrix A using list comprehension
+    c = [i[2] for i in A]
 
     # Collect only the even items of the diagonal of matrix A using list comprehension
+    d= [A[i][i] for i in range(len(A)) if A[i][i]%2==0]
 
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
+    o = [ord(i) for i in p[0]]
 
     return p, r, c, d, o
 
@@ -60,31 +79,31 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 4
     #   color => "green"
-
+    f = {'fruit': 'apple', 'quantity' : 4, 'color': 'green'}
     # Get the item in dictionary f that the key "fruit" maps to
+    a = f['fruit']
 
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
+    f['quantity'] += 1
 
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-
+    d = {'name': {'first_name':'Grace','last_name':'Hopper'}, 'jobs':["scientist", "engineer"], 'age':85}
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
-
+    d['jobs'].append('programmer')
     # Get the third job Grace has that you recently added
+    p = d['jobs'][2]
 
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
+    k = [sorted([i for i in "amazing_grace"])]
 
-    return a, f, p, k
-
-numbers_and_strings()
-lists()
-dictionaries()
+    return a, f, d, p, k
 
 
-
-
-
+print(numbers_and_strings())
+print(lists())
+print(dictionaries())
